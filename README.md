55979dbdfc3d735927665129a75af57c91fc92c4
alexnesov <alexandrenesovic@gmail.com>
2022-06-18 12:59:14 +0200

    yarn init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # React-Sidebar
 React-Sidebar
+
+
+If ```TypeError: cb.apply is not a function```: <br>
+```npm install create-react-app```
---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Sidebar = () => {
+    return (
+        <div>
+
+            Sidebar
+
+        </div>
+    )
+}
+
+export default Sidebar

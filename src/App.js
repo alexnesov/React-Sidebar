9378a00a266528927c35a04e8165c486e3d1714d
alexnesov <alexandrenesovic@gmail.com>
2022-06-18 22:52:06 +0200

    sidebar open impl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Sidebar />
+    <Router>
+        <Sidebar />
+    </Router>
     </>
     );
 }

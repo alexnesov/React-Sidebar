712f63b424770289fc82dd260d0f52769c57d58a
alexnesov <alexandrenesovic@gmail.com>
2022-06-24 19:26:30 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home
 
+from './pages/home';
 function App() {
   return (
     <>
     <Router>
         <Sidebar />
+        <Routes>
+          <Route path="/home" element={<Home/>} />
+        </Routes>
     </Router>
     </>
     );
 }
 
 export default App;
+
+
